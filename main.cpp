baf1ad9c75b375c9b3f2f477e2dec792208d54a8
YungPoves <povesr12@gmail.com>
2025-09-10 10:53:52 +0200

    Add player and alien sprites.

--- main.cpp
+++ main.cpp
@@ -22,6 +22,23 @@ struct Sprite {
   uint8_t *data;
 };
 
+struct Alien {
+  size_t x, y;
+  uint8_t type;
+};
+
+struct Player {
+  size_t x, y;
+  size_t life;
+};
+
+struct Game {
+  size_t width, height;
+  size_t num_aliens;
+  Alien *aliens;
+  Player player;
+};
+
 void buffer_sprite_draw(Buffer *buffer, const Sprite &sprite, size_t x, size_t y, uint32_t color) {
   for(size_t xi = 0; xi < sprite.width; ++xi){
     for(size_t yi = 0; yi < sprite.height; ++yi){
@@ -218,16 +235,50 @@ int main() {
     0,0,0,1,1,0,1,1,0,0,0  // ...@@.@@...
   };
 
-  buffer_sprite_draw(&buffer, alien_sprite, 112, 128, rgb_to_uint32(128, 0, 0));
+  Sprite player_sprite;
+  player_sprite.width = 11;
+  player_sprite.height = 7;
+  player_sprite.data = new uint8_t[77]
+  {
+    0,0,0,0,0,1,0,0,0,0,0, // .....@.....
+    0,0,0,0,1,1,1,0,0,0,0, // ....@@@....
+    0,0,0,0,1,1,1,0,0,0,0, // ....@@@....
+    0,1,1,1,1,1,1,1,1,1,0, // .@@@@@@@@@.
+    1,1,1,1,1,1,1,1,1,1,1, // @@@@@@@@@@@
+    1,1,1,1,1,1,1,1,1,1,1, // @@@@@@@@@@@
+    1,1,1,1,1,1,1,1,1,1,1, // @@@@@@@@@@@
+  };
 
-  // printf("%d\n", clear_color);
+  Game game;
+  game.width = buffer_width;
+  game.height = buffer.height;
+  game.num_aliens = 55;
+  game.aliens = new Alien[game.num_aliens];
+
+  game.player.x = 112 - 5;
+  game.player.y = 32;
+
+  game.player.life = 3;
+
+  for(size_t yi = 0; yi < 5; ++yi){
+    for(size_t xi = 0; xi < 11; ++xi){
+      game.aliens[yi * 11 + xi].x = 16 * xi + 20;
+      game.aliens[yi * 11 + xi].y = 17 * yi + 128;
+    }
+  }
 
   while (!glfwWindowShouldClose(window)) {
     glDrawArrays(GL_TRIANGLES, 0, 3);
 
     glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, buffer.width, buffer.height, GL_RGBA, GL_UNSIGNED_INT_8_8_8_8, buffer.data);
 
-    // glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+    for(size_t ai = 0; ai < game.num_aliens; ++ai)
+    {
+      const Alien& alien = game.aliens[ai];
+      buffer_sprite_draw(&buffer, alien_sprite, alien.x, alien.y, rgb_to_uint32(128, 0, 0));
+    }
+
+    buffer_sprite_draw(&buffer, player_sprite, game.player.x, game.player.y, rgb_to_uint32(128, 0, 0));
 
     glfwSwapBuffers(window);
 
