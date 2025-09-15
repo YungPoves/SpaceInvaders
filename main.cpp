b9d14dcbf459bde17cd436962ea7314ffeb049ea
YungPoves <povesr12@gmail.com>
2025-09-15 14:52:03 +0200

    Refactor.

--- main.cpp
+++ main.cpp
@@ -50,11 +50,11 @@ struct SpriteAnimation {
 };
 
 void buffer_sprite_draw(Buffer *buffer, const Sprite &sprite, size_t x, size_t y, uint32_t color) {
-  for(size_t xi = 0; xi < sprite.width; ++xi){
-    for(size_t yi = 0; yi < sprite.height; ++yi){
+  for (size_t xi = 0; xi < sprite.width; ++xi) {
+    for (size_t yi = 0; yi < sprite.height; ++yi) {
       size_t sy = sprite.height - 1 + y - yi;
       size_t sx = x + xi;
-      if(sprite.data[yi * sprite.width + xi] && sy < buffer->height && sx < buffer->width){
+      if (sprite.data[yi * sprite.width + xi] && sy < buffer->height && sx < buffer->width) {
         buffer->data[sy * buffer->width + sx] = color;
       }
     }
@@ -297,8 +297,8 @@ int main() {
 
   game.player.life = 3;
 
-  for(size_t yi = 0; yi < 5; ++yi){
-    for(size_t xi = 0; xi < 11; ++xi){
+  for (size_t yi = 0; yi < 5; ++yi) {
+    for (size_t xi = 0; xi < 11; ++xi) {
       game.aliens[yi * 11 + xi].x = 16 * xi + 20;
       game.aliens[yi * 11 + xi].y = 17 * yi + 128;
     }
